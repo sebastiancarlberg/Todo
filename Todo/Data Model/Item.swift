d7541a846e2c98647b57bba6d6bfbab9867ce270
Sebastian Carlberg <carlberg.sebastian@gmail.com>
2019-03-02 13:02:15 +0100

    Convert to MVC design pattern

---
+++ Todo/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todo
+//
+//  Created by Sebastian Carlberg on 2019-03-02.
+//  Copyright © 2019 Sebastian Carlberg. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
